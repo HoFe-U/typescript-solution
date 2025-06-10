9800a224bab81904b3e716f5ee32c73a731aca17
[ERP]유호철 <hofe97@douzone.com>
2025-06-10 14:20:47 +0900

    [FEAT] : 함수 타입 / 합수 시그니처 간단 문제

---
+++ practice2/src/todo/todo.ts
@@ -0,0 +1,39 @@
+/**
+ * Todo 1 : 암묵적 anyType 설정
+ */
+
+function func (a,b) { 
+  return;
+}
+
+/**
+ * 아래 빈칸들을 채워보세요
+ * Todo 2
+ */
+
+// 1. 함수 타입 정의
+type Operation = (a: number, b: number) => ______;
+
+// 2. 함수 시그니처(call signature) 형태 정의
+type Operation2 = {
+  (a: number, b: number): ______;
+};
+
+// 3. 오버로딩 함수 선언
+function display(value: string): void;
+function display(value: number): void;
+
+// 실제 구현부
+function display(value: any): void {
+  console.log("VALUE: ", value);
+  // TODO 3: 각기다른 함수가 실행되게 처리
+  value.toLowerCase();
+  value.toFixed();
+}
+
+// 4. 타입 단언
+let unknownValue: unknown;
+let message: string;
+
+unknownValue = "hello";
+message = unknownValue;
