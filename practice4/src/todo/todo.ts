1f068aa6f96167961510fbdf688f8d4bd160dc1b
[ERP]유호철 <hofe97@douzone.com>
2025-06-12 17:33:30 +0900

    [FIX] : 오탈자 수정

--- practice4/src/todo/todo.ts
+++ practice4/src/todo/todo.ts
@@ -75,22 +75,22 @@ type excludeCases = [
 type Include<T, U> = any;
 
 type includeCases = [
-  Expect<Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Kars'>, true>>,
-  Expect<Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'>, false>>,
-  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 7>, true>>,
-  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 4>, false>>,
-  Expect<Equal<Includes<[1, 2, 3], 2>, true>>,
-  Expect<Equal<Includes<[1, 2, 3], 1>, true>>,
-  Expect<Equal<Includes<[{}], { a: 'A' }>, false>>,
-  Expect<Equal<Includes<[boolean, 2, 3, 5, 6, 7], false>, false>>,
-  Expect<Equal<Includes<[true, 2, 3, 5, 6, 7], boolean>, false>>,
-  Expect<Equal<Includes<[false, 2, 3, 5, 6, 7], false>, true>>,
-  Expect<Equal<Includes<[{ a: 'A' }], { readonly a: 'A' }>, false>>,
-  Expect<Equal<Includes<[{ readonly a: 'A' }], { a: 'A' }>, false>>,
-  Expect<Equal<Includes<[1], 1 | 2>, false>>,
-  Expect<Equal<Includes<[1 | 2], 1>, false>>,
-  Expect<Equal<Includes<[null], undefined>, false>>,
-  Expect<Equal<Includes<[undefined], null>, false>>,
+  Expect<Equal<Include<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Kars'>, true>>,
+  Expect<Equal<Include<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'>, false>>,
+  Expect<Equal<Include<[1, 2, 3, 5, 6, 7], 7>, true>>,
+  Expect<Equal<Include<[1, 2, 3, 5, 6, 7], 4>, false>>,
+  Expect<Equal<Include<[1, 2, 3], 2>, true>>,
+  Expect<Equal<Include<[1, 2, 3], 1>, true>>,
+  Expect<Equal<Include<[{}], { a: 'A' }>, false>>,
+  Expect<Equal<Include<[boolean, 2, 3, 5, 6, 7], false>, false>>,
+  Expect<Equal<Include<[true, 2, 3, 5, 6, 7], boolean>, false>>,
+  Expect<Equal<Include<[false, 2, 3, 5, 6, 7], false>, true>>,
+  Expect<Equal<Include<[{ a: 'A' }], { readonly a: 'A' }>, false>>,
+  Expect<Equal<Include<[{ readonly a: 'A' }], { a: 'A' }>, false>>,
+  Expect<Equal<Include<[1], 1 | 2>, false>>,
+  Expect<Equal<Include<[1 | 2], 1>, false>>,
+  Expect<Equal<Include<[null], undefined>, false>>,
+  Expect<Equal<Include<[undefined], null>, false>>,
 ]
 
 /**
